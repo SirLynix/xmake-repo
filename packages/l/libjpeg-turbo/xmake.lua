f950813bbb5ea6d4730ea126dae7362b57af995b
Jérôme Leclercq <lynix680@gmail.com>
2021-02-22 17:16:49 +0100

    libjpeg-turbo: Fix VS Runtime

--- packages/l/libjpeg-turbo/xmake.lua
+++ packages/l/libjpeg-turbo/xmake.lua
@@ -30,6 +30,9 @@ package("libjpeg-turbo")
             table.insert(configs, "-DENABLE_SHARED=OFF")
             table.insert(configs, "-DENABLE_STATIC=ON")
         end
+        if package:is_plat("windows") and package:config("vs_runtime"):startswith("MD") then
+            table.insert(configs, "-DWITH_CRT_DLL=ON")
+        end
         import("package.tools.cmake").install(package, configs)
     end)
 
