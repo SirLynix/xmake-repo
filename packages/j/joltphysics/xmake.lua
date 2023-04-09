a53fdc58920be1d2c3e9c55205703a1c57b82459
Jérôme Leclercq <lynix680@gmail.com>
2023-04-09 15:37:06 +0200

    Fix VS2019 ARM and Android/ARMv7

--- packages/j/joltphysics/xmake.lua
+++ packages/j/joltphysics/xmake.lua
@@ -3,9 +3,10 @@ package("joltphysics")
     set_description("A multi core friendly rigid body physics and collision detection library suitable for games and VR applications.")
     set_license("MIT")
 
-    add_urls("https://github.com/jrouwe/JoltPhysics/archive/refs/tags/$(version).tar.gz",
-             "https://github.com/jrouwe/JoltPhysics.git")
-    add_versions("v2.0.1", "96ae2e8691c4802e56bf2587da30f2cc86b8abe82a78bc2398065bd87dd718af")
+    add_urls("https://github.com/jrouwe/JoltPhysics.git")
+    add_versions("v2.0.1+1", "24147a6936c63f4f09c6c37173169d8f366f9fb4")
+    add_patches("v2.0.1+1", "https://github.com/jrouwe/JoltPhysics/commit/f5a5efc9c2b71c300d65f169cd6c54159b18a080.patch", "f74fd504bcc15b2b49943ba0e2405953c9e57b34dda94c7ac47463ec2f25f58d")
+    add_patches("v2.0.1+1", "https://github.com/jrouwe/JoltPhysics/commit/8ba3e7c52a9255db86cd3e547f5f8f5e2788d15a.patch", "b5fb112d68d38c35d36c9b564a5eefae1bb791b1bfcca0c61f2c4e7970abfbe1")
 
     add_configs("cross_platform_deterministic", { description = "Turns on behavior to attempt cross platform determinism", default = false, type = "boolean" })
     add_configs("debug_renderer", { description = "Adds support to draw lines and triangles, used to be able to debug draw the state of the world", default = true, type = "boolean" })
