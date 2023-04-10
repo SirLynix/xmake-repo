58fdc0fa9b9fc5c6ea38c17d626b97ffd4114e73
Jérôme Leclercq <lynix680@gmail.com>
2023-04-10 15:10:08 +0200

    Update xmake.lua

--- packages/j/joltphysics/xmake.lua
+++ packages/j/joltphysics/xmake.lua
@@ -4,7 +4,7 @@ package("joltphysics")
     set_license("MIT")
 
     add_urls("https://github.com/jrouwe/JoltPhysics.git")
-    add_versions("v2.0.1+1", "24147a6936c63f4f09c6c37173169d8f366f9fb4")
+    add_versions("v2.0.1+1", "8aef496713c398eab4621954b74433826f10d0fd")
     add_patches("v2.0.1+1", "https://github.com/jrouwe/JoltPhysics/commit/f5a5efc9c2b71c300d65f169cd6c54159b18a080.patch", "f74fd504bcc15b2b49943ba0e2405953c9e57b34dda94c7ac47463ec2f25f58d")
     add_patches("v2.0.1+1", "https://github.com/jrouwe/JoltPhysics/commit/8ba3e7c52a9255db86cd3e547f5f8f5e2788d15a.patch", "b5fb112d68d38c35d36c9b564a5eefae1bb791b1bfcca0c61f2c4e7970abfbe1")
 
@@ -74,6 +74,7 @@ package("joltphysics")
             "-DTARGET_PERFORMANCE_TEST=OFF",
             "-DTARGET_SAMPLES=OFF",
             "-DTARGET_VIEWER=OFF",
+            "-DENABLE_ALL_WARNINGS=OFF"
         }
         table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DCROSS_PLATFORM_DETERMINISTIC=" .. (package:config("cross_platform_deterministic") and "ON" or "OFF"))
