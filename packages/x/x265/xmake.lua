2341ef815b2a481b2f4d552b88743a72b97d3e30
SirLynix <lynix680@gmail.com>
2023-05-17 17:10:42 +0200

    Improve x265

--- packages/x/x265/xmake.lua
+++ packages/x/x265/xmake.lua
@@ -1,5 +1,4 @@
 package("x265")
-
     set_homepage("http://x265.org")
     set_description("A free software library and application for encoding video streams into the H.265/MPEG-H HEVC compression format.")
 
@@ -8,20 +7,34 @@ package("x265")
 
     add_versions("3.4", "544d147bf146f8994a7bf8521ed878c93067ea1c7c6e93ab602389be3117eaaf")
 
-    add_deps("cmake", "nasm")
+    add_configs("hdr10_plus", {description = "Enable dynamic HDR10 compilation", default = false, type = "boolean"})
+    add_configs("svt_hevc", {description = "Enable SVT HEVC Encoder", default = false, type = "boolean"})
+
+    add_deps("cmake", "nasm >=2.13")
 
     if is_plat("macosx") then
         add_syslinks("c++")
-    elseif is_plat("linux") then
+    elseif is_plat("linux", "bsd") then
         add_syslinks("pthread", "dl")
     end
 
-    on_install("linux", "macosx", function (package)
-        os.cd("build/linux")
-        os.vrun("./multilib.sh")
-        os.cp("8bit/libx265.a", package:installdir("lib"))
-        os.cp("8bit/x265_config.h", package:installdir("include"))
-        os.cp("../../source/x265.h", package:installdir("include"))
+    on_install(function (package)
+        os.cd("source")
+        if package:is_plat("android") then
+            io.replace("CMakeLists.txt", "list(APPEND PLATFORM_LIBS pthread)", "", { plain = true })
+        end
+        local configs = {}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DENABLE_HDR10_PLUS=" .. (package:config("hdr10_plus") and "ON" or "OFF"))
+        table.insert(configs, "-DENABLE_SVT_HEVC=" .. (package:config("svt_hevc") and "ON" or "OFF"))
+        table.insert(configs, "-DENABLE_SHARED=" .. (package:config("shared") and "ON" or "OFF"))
+        if package:is_plat("windows", "cross") and package:is_arch("arm*") then
+            table.insert(configs, "-DCROSS_COMPILE_ARM=ON")
+        end
+        if package:version() then
+            table.insert(configs, "-DX265_LATEST_TAG=" .. package:version():rawstr())
+        end
+        import("package.tools.cmake").install(package, configs)
     end)
 
     on_test(function (package)
